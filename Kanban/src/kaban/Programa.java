390cc2bb25547819920943ae4eb03140efdf5576
Ro0y <royshakelton@gmail.com>
2014-03-31 08:56:44 -0600

    version 1

--- Kanban/src/kaban/Programa.java
+++ Kanban/src/kaban/Programa.java
@@ -1,19 +1,23 @@
 package kaban;
 
+import java.util.Date;
+
 public class Programa {
+	public static IDashboard dashboard = new DashboardList();
 
 	public static void main(String[] args) throws Exception 
 	{
+		System.out.println("Start" + new Date());
 		Tarea tarea;
-		IDashboard dashboard = new DashboardList();
 		for (int i = 0; i < 15; i++) 
 		{
 			tarea = new Tarea("Tarea " + i, Estado.DO_TO);
-			if (dashboard.agregar(tarea)) 
+			if (tarea.save()) 
 			{
 				System.out.println("Agregada " + tarea.getTitulo());
 			}
 		}
+		System.out.println("End" + new Date());
 	}
 
 }
